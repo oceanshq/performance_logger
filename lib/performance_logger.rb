7629d1cbb006110421e6e17c06c8e5182bde1b2b
Nick Schwaderer <nick.schwaderer@oceanshq.com>
2018-11-29 12:10:56 +0000

    observe correct gemification patterns

--- lib/performance_logger.rb
+++ lib/performance_logger.rb
@@ -6,5 +6,11 @@ require 'performance_logger/performance_logger'
 
 
 module PerformanceLogger
-
+  def self.new(opts)
+    if opts.is_a?(Hash)
+      PLogger.new(opts)
+    else
+      puts "I'm sorry, I don't know what you're trying to pass here!\n\n Please refer to the docs or pass an options hash https://github.com/oceanshq/performance_logger"
+    end
+  end
 end
